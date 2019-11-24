39a65a708ab33fcb9d5e0de8b958643e8fcbe02f
nicolae3421 <43856285+nicolae3421@users.noreply.github.com>
2019-11-24 16:00:08 +0200

    Add files via upload

--- colorX/server/index.js
+++ colorX/server/index.js
@@ -1,31 +1,57 @@
-const color = require('./color')
+// const color = require('./color')
 const fs = require('fs')
 const http = require('http')
 // 102-300x300.jpg
-// color.getPalette('102-5000x5000.jpg')
-//     .then((palette) => {
-//          console.log(palette) 
-//          return {
-//              json: JSON.stringify(palette)
-//          }
-//     })
-//     .then((palette) => { 
-//         fs.writeFile('../public/images_data/102-5000x5000.jpg.json', palette.json, null)
-//     })
-//     .then( () => {
-//         console.log('Done')
-//     })
+// try{
+//     color.getPalette('102-5000x5000.jpg')
+//         .then((palette) => {
+//             console.log(palette)
+//             return {
+//                 json: JSON.stringify(palette)
+//             }
+//         })
+//         .then((palette) => {
+//             fs.writeFile('../public/images_data/102-5000x5000.jpg.json', palette.json, null)
+//         })
+//         .then(() => {
+//             console.log('Done')
+//         })
+// }catch(error){
+//     console.log(error)
+// }
+
 
 const server = http.createServer((req, res) => {
-    if( req.url == '/image' ){
-        var data = fs.readFileSync('../public/images/102-300x300.jpg')
-        res.write(data)
-    }else if( req.url == '/data') {
-        var data = fs.readFileSync('../public/images_data/102-300x300.jpg.json')
-        res.end(data)
-    }else{
+
+    if ( result = req.url.match(/\/image\/(.*\.jpg)/)) {
+        try{
+            var data = fs.readFileSync(`../public/images/${result[1]}`)
+            res.end(data)
+        }catch(error){
+            res.end('Page not found!')
+        }
+        
+    } else if ( (result = req.url.match(/\/data\/(.*\.json)/) )) {
+        try{
+            var data = fs.readFileSync(`../public/images_data/${result[1]}`)
+            res.end(data)
+        }catch(error){
+            res.end('Page not found!')
+        }
+        
+    } else if (req.url = '/') {
+        try {
+            var data = fs.readFileSync(`../public/index.html`)
+            res.end(data)
+        } catch (error) {
+            res.end('Page not found!')
+        }
+
+    } else {
         res.end("It is not working :(")
     }
+
     
+
 })
-server.listen(8000)
+server.listen(3000)
