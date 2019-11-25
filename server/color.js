99ef97ff9e910f05ede69ef331e14372b0157bc9
nicolae3421 <43856285+nicolae3421@users.noreply.github.com>
2019-11-25 22:23:45 +0200

    Delete color.js

--- server/color.js
+++
@@ -1,7 +0,0 @@
-// module with color tools
-const Vibrant = require('node-vibrant')
-
-exports.getPalette = ( image_name ) => {
-    return Vibrant.from(`../public/images/${image_name}`)
-        .getPalette()
-}
