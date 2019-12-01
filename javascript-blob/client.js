0d56ce70fb13d4e0580cb0bc8d0296d26e575388
nicolae3421 <43856285+nicolae3421@users.noreply.github.com>
2019-12-01 15:59:06 +0200

    Add files via upload

---
+++ javascript-blob/client.js
@@ -0,0 +1,30 @@
+function load (){
+
+    // CORS policy
+
+    fetch('http://127.0.0.1:3000')
+        .then( response=>{
+            return response.json()
+        })
+        .then( json => {
+            let container = document.querySelector('#result')
+            let h1 = document.createElement('h1')
+                h1.innerText = json.title
+            let author = document.createElement('p')
+                author.innerText = json.author
+            let data = document.createElement('p')
+                data.innerText = json.data
+
+            // let img_data = Buffer.from(json.image.data)
+
+            let image = document.createElement('img')
+                // image.src = 'data:image/jpeg;base64,' + img_data.toString('base64')
+            // console.log(json)
+
+            container.appendChild(h1)
+            container.appendChild(author)
+            container.appendChild(data)
+            container.appendChild(image)
+
+        })
+}
---
+++ javascript-blob/server.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+
+
+    let data = fs.readFileSync('./data/image.json')
+    // data -> Buffer -> String(JSON) -> Object
+    //                   String(JSON) <- Object
+    data_obj = JSON.parse(data.toString())
+    data_obj.image = fs.readFileSync('./data/image.jpg')
+    console.log(data_obj)
+
+    res.setHeader('Content-type', 'aplication/json')
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+
+    res.end( JSON.stringify(data_obj) ) // aici bufferu se strica
+})
+server.listen(3000)
